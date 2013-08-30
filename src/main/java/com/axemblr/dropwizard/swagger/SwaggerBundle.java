c9f0b464cd48445e766553adb965b7174dd8fd6c
Niklas Herder <niklas.herder@aftonbladet.se>
2013-08-30 12:10:18 +0200

    Remove .json suffix and enable configuration of URI path via SwaggerBundle constructor.

--- src/main/java/com/axemblr/dropwizard/swagger/SwaggerBundle.java
+++ src/main/java/com/axemblr/dropwizard/swagger/SwaggerBundle.java
@@ -1,21 +1,39 @@
 package com.axemblr.dropwizard.swagger;
 
-import com.yammer.dropwizard.bundles.AssetsBundle;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.jaxrs.JaxrsApiReader;
+import com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Environment;
 
-import com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 public class SwaggerBundle extends AssetsBundle {
 
-    public static final String DEFAULT_PATH = "/swagger-ui";
+	public static final String DEFAULT_PATH = "/swagger-ui";
 
-    public SwaggerBundle() {
-        super(DEFAULT_PATH);
-    }
+	public SwaggerBundle() {
+		this(DEFAULT_PATH);
+	}
 
-    @Override
-    public void initialize(Environment environment) {
-        super.initialize(environment);
-        environment.addResource(new ApiListingResourceJSON());
-    }
+	public SwaggerBundle(String uriPath) {
+		super(DEFAULT_PATH, uriPath, "index.html");
+	}
+
+
+	@Path("/api-docs")
+	@Api("/api-docs")
+	@Produces({"application/json"})
+	class ApiListingResourceJSONWithoutSuffix extends ApiListingResourceJSON {
+		public ApiListingResourceJSONWithoutSuffix() {
+			JaxrsApiReader.setFormatString("");
+		}
+	}
+
+	@Override
+	public void run(Environment environment) {
+		super.run(environment);
+		environment.addResource(new ApiListingResourceJSONWithoutSuffix());
+	}
 }
--- src/test/java/com/axemblr/dropwizard/swagger/resources/PetResource.java
+++ src/test/java/com/axemblr/dropwizard/swagger/resources/PetResource.java
@@ -4,13 +4,14 @@ import com.axemblr.dropwizard.swagger.core.Pet;
 import com.google.common.collect.ImmutableSet;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Set;
 
-@Path("/pet.json")
+@Path("/pet")
 @Api("/pet")
 @Produces(MediaType.APPLICATION_JSON)
 public class PetResource {
